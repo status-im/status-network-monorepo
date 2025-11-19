9cd06de9284d938e95efe48998997256dfb28f8a
sydhds <sylvain@status.im>
2025-11-19 10:21:11 +0100

    Add prover_pmtree + prover_merkle_tree crates

--- rln-prover/prover/src/user_db_error.rs
+++ rln-prover/prover/src/user_db_error.rs
@@ -96,6 +96,22 @@ pub enum UserTierInfoError<E: std::error::Error> {
 
 // UserDb2
 
+#[derive(thiserror::Error, Debug)]
+pub enum RegisterError2 {
+    #[error("User (address: {0:?}) has already been registered")]
+    AlreadyRegistered(Address),
+    #[error(transparent)]
+    Db(#[from] DbErr),
+    #[error("Too many users, exceeding merkle tree capacity...")]
+    TooManyUsers,
+    #[error("Merkle tree error: {0}")]
+    TreeError(ZerokitMerkleTreeError),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    FromConfig(#[from] FromConfigError),
+}
+
 #[derive(thiserror::Error, Debug, PartialEq)]
 pub enum TxCounterError2 {
     #[error("User (address: {0:?}) is not registered")]
