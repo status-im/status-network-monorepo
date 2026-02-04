24260f5e9fc7a7f0846ca309bcf5ebdecc8a089f
nadeemb53 <nadeemb53@gmail.com>
2026-02-04 17:21:13 +0800

    feat: Implement logical replication for RLN Prover DB

--- docker/postgres/init-replica/init-replica.sh
+++ docker/postgres/init-replica/init-replica.sh
@@ -7,7 +7,21 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -U $POSTGRES_USER -d prover
   sleep 1
 done
 
->&2 echo "Primary Postgres is up - executing command"
+>&2 echo "Primary Postgres is up"
+
+# Logic to ADD tables to publication after they are created by the application (rln-prover)
+# We loop until the tables exist on the Primary
+MAX_RETRIES=30
+for i in $(seq 1 $MAX_RETRIES); do
+    if PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -U $POSTGRES_USER -d prover_db -c "SELECT 'tx_counter'::regclass;" >/dev/null 2>&1; then
+        echo "Table tx_counter found on Primary. Adding to publication..."
+        PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -U $POSTGRES_USER -d prover_db -c "ALTER PUBLICATION rln_publication ADD TABLE tx_counter, deny_list;" || true
+        break
+    else
+        echo "Waiting for rln-prover to create tables on Primary... ($i/$MAX_RETRIES)"
+        sleep 2
+    fi
+done
 
 # Create Database if it doesn't exist (it enters here as default postgres db)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
--- docker/postgres/init/01-init-publication.sql
+++ docker/postgres/init/01-init-publication.sql
@@ -1,2 +1,4 @@
--- Enable logical replication for the required tables
-CREATE PUBLICATION rln_publication FOR TABLE tx_counter, deny_list;
+-- Enable logical replication
+-- We create an empty publication first. Tables will be added later once they are created by the application/migration.
+-- This prevents startup failure because headers (tx_counter, etc.) do not exist yet.
+CREATE PUBLICATION rln_publication;
